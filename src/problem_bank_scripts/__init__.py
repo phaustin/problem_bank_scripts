d5e77386583ac7df473ef1f6be3ba24f05c2eae7
Phil Austin <paustin@eos.ubc.ca>
2022-03-02 11:49:01 -0800

    add VERSSION.txt to manifest

--- src/problem_bank_scripts/__init__.py
+++ src/problem_bank_scripts/__init__.py
@@ -1,3 +1,19 @@
 __version__ = "0.2.13"
 
 from .problem_bank_scripts import *
+from pathlib import Path
+
+root_dir = Path(__file__).resolve().parent
+# print(f"{root_dir=}")
+version_file= root_dir / 'VERSION.txt'
+
+if not version_file.is_file():
+    __version__ = 'no_version'
+    try:
+        with open(version_file,'w') as f:
+            f.write(__version__)
+    except:
+        __version_file__=None
+else:
+    with open(version_file) as f:
+        __version__=f.read().strip()
